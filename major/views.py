42e2d4f74351c1a531b20aab146c03697b1de3e9
xiongkai <1249235131@qq.com>
2019-11-12 21:38:54 +0800

    增加异常处理

--- major/views.py
+++ major/views.py
@@ -6,14 +6,18 @@ import os
 
 def index(request):
     if request.method == "POST":
-        file_obj = request.FILES.get("up_image")  # 获取图片
-        state = request.POST.get("state")  # 获取state参数值
-        print(state)
-        file_name = file_obj.name
-        f1 = open(file_name, "wb")
-        for i in file_obj.chunks():
-            f1.write(i)
-        f1.close()
-        # os.remove(file_name)  # 用完请删除
+        try:
+            file_obj = request.FILES.get("up_image")  # 获取图片
+            state = request.POST.get("state")  # 获取state参数值
+            print(state)
+            file_name = file_obj.name
+            f1 = open(file_name, "wb")
+            for i in file_obj.chunks():
+                f1.write(i)
+            f1.close()
+            # os.remove(file_name)  # 用完请删除
+        except Exception as e:
+            return JsonResponse({"result": "System Error!\n" + repr(e)})
+        
         return JsonResponse({"result": "hahaha"})
     return render(request, "index.html")
